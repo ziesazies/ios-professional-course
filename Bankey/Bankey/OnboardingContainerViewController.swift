c8f1d27fc13b5528bd73cb795759b7a9bb0b2d7b
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2024-05-29 16:27:46 +0700

    refactor: create generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused accros multiple onboarding screens.

--- Bankey/Bankey/OnboardingContainerViewController.swift
+++ Bankey/Bankey/OnboardingContainerViewController.swift
@@ -19,9 +19,13 @@ class OnboardingContainerViewController: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
+//        let page1 = ViewController1()
+//        let page2 = ViewController2()
+//        let page3 = ViewController3()
+        
+        let page1 = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989.")
+        let page2 = OnboardingViewController(heroImageName: "thumbs", titleText: "Move your money around the world quickly and securely")
+        let page3 = OnboardingViewController(heroImageName: "world", titleText: "Learn more at www.bankey.com")
         
         pages.append(page1)
         pages.append(page2)
@@ -91,26 +95,4 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
     }
     
     
-}
-
-// MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-    }
 }
