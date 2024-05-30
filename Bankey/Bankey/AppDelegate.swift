a8e83877c6112a3083e98f620f060c8fd092e21e
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2024-05-30 19:10:29 +0700

    refactor: change from SceneDelegate to AppDelegate only

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -7,45 +7,70 @@
 
 import UIKit
 
-@main
+let appColor: UIColor = .systemTeal
+
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    
     let loginViewController = LoginViewController()
-
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    let dummyViewController = DummyViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        let window = UIWindow(windowScene: windowScene)
+        //        let window = UIWindow(windowScene: windowScene)
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         window.makeKeyAndVisible()
         window.backgroundColor = .systemBackground
-        
         loginViewController.delegate = self
-        
+        onboardingContainerViewController.delegate = self
+        dummyViewController.logoutDelegate = self
         window.rootViewController = loginViewController
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+}
+    
+extension AppDelegate {
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.7,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        print("Did login")
+        if LocalState.hasOnboarded {
+           setRootViewController(dummyViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
     }
 }
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        LocalState.hasOnboarded = true
+        setRootViewController(dummyViewController)
+    }
+}
+
+extension AppDelegate: LogoutDelegate {
+    func didLogout() {
+        setRootViewController(loginViewController)
+    }
+}
+
---
+++ Bankey/Bankey/UIViewController+Utils.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Alief Ahmad Azies on 30/05/24.
+//
+
+import Foundation
